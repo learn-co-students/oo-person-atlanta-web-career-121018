5c96ed0f8f0f848a2f85e5eac310ab2fa8482c10
Sarah <sarahyun7@gmail.com>
2018-12-15 22:33:03 -0500

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,8 @@ require "pry"
 
 class Person
   attr_reader :name, :happiness, :hygiene
-
+  #name is here because we don't want to be able to overwrite name
+  #happiness & hygiene are here because we set happiness= & hygiene= later
   attr_accessor :bank_account
 
 
@@ -11,6 +12,7 @@ class Person
     @bank_account = 25
     @happiness = 8
     @hygiene = 8
+    #every new instance of a person is instantiated with these
   end
 
   def happiness=(num)
@@ -20,6 +22,7 @@ class Person
     elsif num < 0
       @happiness = 0
     end
+    #if happiness exceeds 10, happiness = 10 // below 0 = 0
   end
 
   def hygiene=(num)
@@ -33,6 +36,7 @@ class Person
 
   def happy?
     @happiness > 7
+    #returns true if happiness >7
   end
 
   def clean?
@@ -47,6 +51,7 @@ class Person
   def take_bath
     @hygiene += 4
     self.hygiene = @hygiene
+    #calls on hygiene= method to increment hygiene & uses 0-10 rule
     "♪ Rub-a-dub just relaxing in the tub ♫"
   end
 
@@ -55,6 +60,7 @@ class Person
     @hygiene -= 3
     self.happiness = @happiness
     self.hygiene = @hygiene
+    #calls on happiness= & hygiene= methods
     "♪ another one bites the dust ♫"
   end
 
@@ -70,6 +76,7 @@ class Person
     if topic == "politics"
       self.happiness -= 2
       person.happiness -= 2
+      #using 'person.' which will take the other person (who has a class Person), call their happiness level & decrease by 2
       "blah blah partisan blah lobbyist"
     elsif topic == "weather"
       self.happiness += 1
